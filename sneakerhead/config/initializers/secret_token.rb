849b11090a6b5a6596b5ab9682398a70957c46c2
Waley Chen <Phoenix@Waleys-MacBook-Air.local>
2014-05-31 13:35:21 -0400

    rails

---
+++ sneakerhead/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sneakerhead::Application.config.secret_key_base = 'b3f05eb00de1cda6f5bcc733abc5943e560ad609d9eaf10cc8914f74ca39912765eac7ee256a3d3e8d296641560d8dc2797c2375a3591759f458bd0df776c2dc'
