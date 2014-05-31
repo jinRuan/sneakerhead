34a6b032fcf663e9f660d03a8c7843663a8a2762
jinRuan <jruan6377@bths.edu>
2014-05-31 15:55:19 -0400

    added  bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def application
+  def dashboard
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root to: 'application#application'
+  root to: 'application#dashboard'
 end
